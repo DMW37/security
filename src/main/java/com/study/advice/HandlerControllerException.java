dceb0332002427233a950787ba03c69aa8092261
deng Mingwei <3561294402@qq.com>
2022-02-15 21:33:58 +0800

    集中式SpringBoot整合security 异常页面处理(页面跳转路径完善)

--- src/main/java/com/study/advice/HandlerControllerException.java
+++ src/main/java/com/study/advice/HandlerControllerException.java
@@ -1,21 +1,22 @@
 package com.study.advice;
 
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * @author 35612
  */
-@RestControllerAdvice
+@ControllerAdvice
 public class HandlerControllerException {
 
     @ExceptionHandler(RuntimeException.class)
     public String  handlerException(RuntimeException e){
         if (e instanceof AccessDeniedException){
-            return "redirect:403.jsp";
+            return "redirect:/403.jsp";
         }else {
-            return "forward:500.jsp";
+            return "forward:/500.jsp";
         }
     }
 }
