9f7e19c495a60d8bf690b4fb78aa3b042f3588b0
deng Mingwei <3561294402@qq.com>
2022-02-15 21:53:17 +0800

    集中式SpringBoot整合security 记住密码实现

--- src/main/java/com/study/config/SecurityConfig.java
+++ src/main/java/com/study/config/SecurityConfig.java
@@ -10,22 +10,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+
+import javax.sql.DataSource;
 
 /**
  * @author 35612
  */
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true,jsr250Enabled = true,prePostEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private SysUserService sysUserService;
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Autowired
+    private DataSource dataSource;
 
     /**
      * 配置认证方式[内存或者数据库]
@@ -57,10 +65,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/product").hasAnyRole("USER").anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login.jsp").loginProcessingUrl("/login").successForwardUrl("/index.jsp").failureForwardUrl("/failer.jsp")
+                .and().rememberMe().rememberMeServices(rememberMeServices()).key("INTERNAL_SECRET_KEY")
                 .and()
                 .logout().logoutSuccessUrl("/logout").invalidateHttpSession(true).logoutSuccessUrl("/login.jsp")
                 .and()
                 .csrf().disable();
 
     }
+
+    /**
+     * 返回 RememberMeServices 实例
+     *
+     * @return the remember me services
+     */
+    @Bean
+    public RememberMeServices rememberMeServices() {
+        JdbcTokenRepositoryImpl rememberMeTokenRepository = new JdbcTokenRepositoryImpl();
+        // 此处需要设置数据源，否则无法从数据库查询验证信息
+        rememberMeTokenRepository.setDataSource(dataSource);
+
+        // 此处的 key 可以为任意非空值(null 或 "")，单必须和起前面
+        // rememberMeServices(RememberMeServices rememberMeServices).key(key)的值相同
+        PersistentTokenBasedRememberMeServices rememberMeServices =
+                new PersistentTokenBasedRememberMeServices("INTERNAL_SECRET_KEY", sysUserService, rememberMeTokenRepository);
+
+        // 该参数不是必须的，默认值为 "remember-me", 但如果设置必须和页面复选框的 name 一致
+        rememberMeServices.setParameter("remember-me");
+        rememberMeServices.setTokenValiditySeconds(180);
+        return rememberMeServices;
+    }
+
 }
